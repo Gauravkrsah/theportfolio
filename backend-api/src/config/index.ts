2c148f7c11ed854115b8bcba298b4a37145223cd
Gauravkrsah <gauravkumarsah.078@kathford.edu.np>
2025-05-20 22:52:14 +0545

    seting up

--- backend-api/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Centralized configuration exports
- */
-export * from './environment';
-
-// Export a default validate function to check if all required configs are available
-export { validateConfig as default } from './environment';
